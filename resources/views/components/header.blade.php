e263f5a53df4566920e035070701216dbcda3583
bmprojectbeats <bmprojectbeats@yandex.ru>
2024-02-02 12:30:15 +0500

    index

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,32 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<link rel="stylesheet" href="./css/style.css">
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid center">
+          <a class="navbar-brand" href="#">Котофилия</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Переключатель навигации">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="./index.html">О нас</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./app.html">Точить когти</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./log.html">Вход</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./reg.html">Регистрация</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./cab.html">Кабинет</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="">Выход</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Котофилия</title>
+</head>
+<body>
+<x-header/>
+      <div class="main center">
+        <div class="main__info">
+            <p class="title2">
+                BYajhsjis sjkaosk sakkasopsd asdkoad
+            </p>
+            <button class="info__button"> Pspdls;d </button>
+        </div>
+        <div class="main_img">
+            <img src="./images/1.png" alt="ss" style="max-width: 300px;">
+        </div>
+      </div>
+      <div class="main center"> 
+        <div class="main_img">
+            <img src="./images/2.jpg" alt="ss" style="max-width: 300px;">
+        </div>
+        <div class="main__info">
+            <p class="title1">
+               O nas
+            </p>
+            <p class="title2">
+                BYajhsjis sjkaosk sakkasopsd asdkoad
+            </p>
+        </div>
+        </div>
+        <div class="cats center"> 
+                <div class="cats_info">
+                <p class="title1">
+                  Nashi cats
+                </p>
+                <div class="filter__a">
+                    <a href="#" class="filter_a">Smeshnie cats</a>
+                <a href="#" class="filter_a">Grustni cats</a>
+                <a href="#" class="filter_a">Tupie cats</a>
+                </div>
+                </div>  
+                <div class="cats__cards">
+                    <div class="card" style="width: 18rem;">
+                        <img src="./images/1.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">Заголовок карточки</h5>
+                          <p class="card-text">Небольшой пример текста, который должен основываться на названии карточки и составлять основную часть содержимого карты.</p>
+           
+                        </div>
+                      </div>
+                      <div class="card" style="width: 18rem;">
+                        <img src="./images/1.png" class="card-img-top cats__img" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">Заголовок карточки</h5>
+                          <p class="card-text">Небольшой пример текста, который должен основываться на названии карточки и составлять основную часть содержимого карты.</p>
+           
+                        </div>
+                      </div>
+                      <div class="card" style="width: 18rem;">
+                        <img src="./images/3.jpg" class="card-img-top cats__img" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">Заголовок карточки</h5>
+                          <p class="card-text">Небольшой пример текста, который должен основываться на названии карточки и составлять основную часть содержимого карты.</p>
+           
+                        </div>
+                      </div>
+                </div>
+                
+            </div>
+            </div>
+     <x-js/>
+</body>
+</html>
